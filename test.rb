b847a295243f71f0e5190c933206d040770eda65
tosbourn <toby.osbourn@googlemail.com>
2015-02-19 21:28:21 +0000

    Tidy up quick test file

--- test.rb
+++ test.rb
@@ -3,5 +3,5 @@ require_relative "spaceship"
 world = World.new
 
 360.times do |deg|
-  puts "#{deg} | #{world.trajectory(deg, 10)} | #{Math.cos(deg)} | #{Math.sin(deg)}"
+  puts "#{deg} | #{world.trajectory(deg, 10)}"
 end
